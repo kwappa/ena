2683290b5dce99f1a91381add59563276b5f29bf
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-24 08:06:56 +0900

    add Users#show
    * add scope to route by '/:nick'
    * add controller and template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def show
+    if request.path_info.start_with?('/users')
+      @user = current_user
+    else
+      @user = User.find_by(nick: params[:nick])
+    end
+    raise ActiveRecord::RecordNotFound unless @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
 
   devise_for :users
+
+  scope ':nick' do
+    get '/', controller: :users, action: :show
+  end
 end
