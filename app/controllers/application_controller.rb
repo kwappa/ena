cb542c3370aa520a8d8eb6f3ab07ce3c0e2368af
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-23 21:35:18 +0900

    implement sign_up form
    * add fields to sign up form
    * add permited columns for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  PERMITED_COLUMNS_FOR_USER = [
+    :name,
+    :nick,
+    :member_number,
+    :screen_name,
+    :screen_name_kana,
+  ]
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    PERMITED_COLUMNS_FOR_USER.each do |column|
+      devise_parameter_sanitizer.for(:sign_up) << column
+      devise_parameter_sanitizer.for(:sign_in) << column
+      devise_parameter_sanitizer.for(:account_update) << column
+    end
+  end
+
   protect_from_forgery with: :exception
 end
