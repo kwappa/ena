7e692962321bc15fcdcab0bfb983b5a2378503fc
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-23 11:37:02 +0900

    install factory_girl_rails

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
 
 group :test do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,4 +9,5 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.include FactoryGirl::Syntax::Methods
 end
