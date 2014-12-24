a28bbec0725d76380003adbf5bdf57dddf472f9f
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-24 22:22:56 +0900

    modify and execute migration

--- db/migrate/20141224130816_create_user_profiles.rb
+++ db/migrate/20141224130816_create_user_profiles.rb
@@ -1,10 +1,11 @@
 class CreateUserProfiles < ActiveRecord::Migration
   def change
     create_table :user_profiles do |t|
-      t.integer, :user_id
-      t.text :body
-
-      t.timestamps null: false
+      t.integer  :user_id, null: false
+      t.text     :body
+      t.timestamps         null: false
     end
+
+    add_index :user_profiles, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222224245) do
+ActiveRecord::Schema.define(version: 20141224130816) do
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_profiles", ["user_id"], name: "index_user_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   null: false
