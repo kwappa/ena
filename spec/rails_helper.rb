e157b94e1d9ae1c17e578d51f85eb0189d7e7564
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-25 02:15:52 +0900

    include Devise::TestHelpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,4 +10,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
 end
