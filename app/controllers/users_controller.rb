b7cac64016e0a382bfec10fcb9729f696cc76655
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-01-07 19:27:28 +0900

    implement UsersController#list
    * specify order from query parameters
    * add spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  MEMBERS_PER_PAGE = 10
+
   layout 'users'
 
   def index
@@ -18,6 +20,13 @@ class UsersController < ApplicationController
 
   def list
     @user = current_user
+    relation = case params[:sort]
+               when 'recent' then User.recent
+               when 'member_number' then User.order_by_member_number
+               else User.order_by_nick
+               end
+    relation = relation.reverse_order if params[:order] == 'desc'
+    @members = relation.page(params[:page]).per(MEMBERS_PER_PAGE)
   end
 
   def after_edit
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  let(:user) { create(:user) }
-
   describe '#index' do
+    let(:user) { create(:user) }
     subject(:index) { get :index }
     context 'when user does not login' do
       it { expect(index).to be_ok }
@@ -12,6 +11,44 @@ RSpec.describe UsersController, type: :controller do
     context 'when user login' do
       before { sign_in user }
       it { expect(index).to redirect_to home_path(user.nick) }
+    end
+  end
+
+  describe '#list' do
+    let(:params) { {} }
+    let(:per_page) { UsersController::MEMBERS_PER_PAGE }
+    let(:member_numbers) { (0..per_page).to_a.shuffle }
+    let(:edit_orders)    { (0..per_page).to_a.shuffle }
+    let!(:members)       { (0..per_page).map { |idx| create(:user, member_number: member_numbers[idx]) } }
+    before { get :list, params }
+
+    context 'default sort' do
+      it 'sorted by alphabetically order' do
+
+        expect(assigns[:members].pluck(:id)).to match User.order_by_nick.pluck(:id).first(per_page)
+      end
+
+      context 'with option "order=desc"' do
+        let(:params) { { order: 'desc' } }
+        it 'reverse sorted by alphabetically order' do
+          expect(assigns[:members].pluck(:id)).to match User.order_by_nick.reverse_order.pluck(:id).first(per_page)
+        end
+      end
+    end
+
+    context 'sorted by member_number' do
+      let(:params) { { sort: 'member_number' } }
+      it 'sorted by member_number' do
+        expect(assigns[:members].pluck(:id)).to match User.order_by_member_number.pluck(:id).first(per_page)
+      end
+    end
+
+    context 'recent' do
+      let(:params) { { sort: 'recent' } }
+      before { edit_orders.map { |idx| members[idx].touch } }
+      it 'sorted by updated_at desc' do
+        expect(assigns[:members].pluck(:id)).to match User.recent.pluck(:id).first(per_page)
+      end
     end
   end
 end
