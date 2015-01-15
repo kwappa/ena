400632e9f31641c7a4ffa9243ae03864bba7ed52
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-01-15 19:02:35 +0900

    implement UserTag.normalize_keyword

--- app/models/user_tag.rb
+++ app/models/user_tag.rb
@@ -1,4 +1,10 @@
 class UserTag < ActiveRecord::Base
+  require 'nkf'
+
   has_many :user_taggings
   has_many :users, through: :user_taggings
+
+  def self.normalize_keyword(keyword)
+    NKF.nkf('-w -X -Z', keyword.gsub(/[\s　]/, ''))
+  end
 end
--- spec/models/user_tag_spec.rb
+++ spec/models/user_tag_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe UserTag, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe UserTag, type: :model do
+  describe '.normalize_keyword' do
+    let(:keyword) { 'keyword' }
+    subject(:result) { described_class.normalize_keyword(keyword) }
+
+    context 'includes blank' do
+      let(:keyword) { "A B　C\rD\nE\tF\fG\vH" }
+      it 'removes blank characters and full-width blank' do
+        expect(result).to eq 'ABCDEFGH'
+      end
+    end
+
+    context 'includes HIRAGANA / KATAKANA' do
+      let(:keyword) { '漢字ｶﾀｶﾅひらがなカタカナ' }
+      it 'converts hankaku-kana to zenkaku-kana' do
+        expect(result).to eq '漢字カタカナひらがなカタカナ'
+      end
+    end
+
+    context 'includes ZENKAKU alphabet and mark' do
+      let(:keyword) { 'ｊａｖａｓｃｒｉｐｔ　ｆｕｎｃｔｉｏｎ（）' }
+      it 'converts to ascii' do
+        expect(result).to eq 'javascriptfunction()'
+      end
+    end
+  end
 end
