9f2de4293254d31fd2c023e43f54a5024016c91f
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-01-22 17:42:34 +0900

    show suspend status on user list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def gravatar_image_tag(user)
     image_tag("http://gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", class: 'gravatar_icon')
   end
+
+  def user_suspend_icon_tag(user)
+    return "" if user.active?
+    content_tag :i, "", class: "fa fa-#{user.suspend_status_fa_icon} fa-lg"
+  end
 end
