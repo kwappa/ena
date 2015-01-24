9e9823a8f777210f3438c459085f1fb2b12b04c9
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-01-24 14:26:35 +0900

    implement list of teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,13 @@
 class Team < ActiveRecord::Base
+  include Renderable
+
   validates_presence_of :name
+
+  def active?
+    self.disbanded_on.blank?
+  end
+
+  def disbanded?
+    self.active?.!
+  end
 end
